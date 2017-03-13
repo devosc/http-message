30bea7b3f8d9c62da0af2d827f6ec4c5dc86ea8c
devosc <devosc@gmail.com>
2017-03-12 23:02:16 -0500

    tidy up

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -7,9 +7,8 @@ namespace Valar\Request;
 
 use Mvc5\Request\Request as Mvc5Request;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\RequestInterface;
 
 interface Request
-    extends Mvc5Request, RequestInterface, ServerRequestInterface
+    extends Mvc5Request, ServerRequestInterface
 {
 }
