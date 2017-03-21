7eac97360947b8ae90884b9116d0a45c5d42a1fd
devosc <devosc@gmail.com>
2017-03-20 20:59:17 -0500

    fix body

--- src/Plugin/Body.php
+++ src/Plugin/Body.php
@@ -5,7 +5,7 @@
 
 namespace Valar\Plugin;
 
-use Mvc5\Plugin\Plugin;
+use Mvc5\Plugin\ScopedCall;
 use Mvc5\Plugin\Shared;
 use Zend\Diactoros\PhpInputStream;
 
@@ -17,6 +17,16 @@ class Body
      */
     function __construct($name = 'body')
     {
-        parent::__construct($name, new Plugin(PhpInputStream::class));
+        parent::__construct($name, new ScopedCall($this));
+    }
+
+    /**
+     * @return \Closure
+     */
+    function __invoke()
+    {
+        return function() {
+            return new PhpInputStream;
+        };
     }
 }
