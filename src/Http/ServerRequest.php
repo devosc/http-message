c126be24c48006e91bbcfb54586a7029729f0da0
devosc <devosc@gmail.com>
2017-03-29 07:28:33 -0500

    allow string object

--- src/Http/ServerRequest.php
+++ src/Http/ServerRequest.php
@@ -31,17 +31,23 @@ class ServerRequest
 
         $server = $config[Arg::SERVER];
 
+        !isset($config[Arg::HEADERS]) &&
+            $config[Arg::HEADERS] = Factory::marshalHeaders($server);
+
+        is_array($config[Arg::HEADERS]) &&
+            $config[Arg::HEADERS] = new HttpHeaders($config[Arg::HEADERS]);
+
+        !isset($config[Arg::URI]) && $config[Arg::URI] = new Uri(
+            Factory::marshalUriFromServer($server, \iterator_to_array($config[Arg::HEADERS]))
+        );
+
         !isset($config[Arg::ARGS]) && $config[Arg::ARGS] = $_GET;
         !isset($config[Arg::ATTRIBUTES]) && $config[Arg::ATTRIBUTES] = new Attributes;
         !isset($config[Arg::BODY]) && $config[Arg::BODY] = new PhpInputStream;
         !isset($config[Arg::COOKIES]) && $config[Arg::COOKIES] = $_COOKIE;
         !isset($config[Arg::DATA]) && $config[Arg::DATA] = $_POST;
         !isset($config[Arg::FILES]) && $config[Arg::FILES] = Factory::normalizeFiles($_FILES);
-        !isset($config[Arg::HEADERS]) && $config[Arg::HEADERS] = new HttpHeaders(Factory::marshalHeaders($server));
         !isset($config[Arg::METHOD]) && $config[Arg::METHOD] = $server['REQUEST_METHOD'] ?? 'GET';
-        !isset($config[Arg::URI]) && $config[Arg::URI] = new Uri(
-            (string) Factory::marshalUriFromServer($server, \iterator_to_array($config[Arg::HEADERS]))
-        );
         !isset($config[Arg::VERSION]) && $config[Arg::VERSION] = substr($server['SERVER_PROTOCOL'] ?? 'HTTP/1.1', strlen('HTTP/'));
 
         parent::__construct($config);
