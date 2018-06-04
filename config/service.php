17ecf0d1d04421149d40fd390929007cc14417ea
devosc <devosc@gmail.com>
2018-06-03 22:16:22 -0500

    json error and exception response

--- config/service.php
+++ config/service.php
@@ -3,17 +3,25 @@
  *
  */
 
-use Mvc5\Plugin\GlobalVar;
-use Mvc5\Plugin\Plugin;
+use Mvc5\Plugin\Param;
 use Valar\Plugin\ServerRequest;
 
 return [
-    'request' => new ServerRequest(include __DIR__ . '/request.php'),
     //'request' => Valar\Http\ServerRequest::class,
-    'response' => Valar\Response::class,
+    'request' => new ServerRequest(include __DIR__ . '/request.php'),
+
     //'response' => Valar\Http\Response::class,
-    'response\json' => Valar\JsonResponse::class,
+    'response' => Valar\Response::class,
+
     //'response\json' => Valar\Http\JsonResponse::class,
-    'response\redirect' => Valar\RedirectResponse::class,
+    'response\json' => Valar\JsonResponse::class,
+
+    //'response\json\error'  => Valar\Http\JsonErrorResponse::class,
+    'response\json\error'  => Valar\JsonErrorResponse::class,
+
+    //'response\json\exception' => [Valar\Http\JsonExceptionResponse::class, 'trace' => new Param('debug')],
+    'response\json\exception' => [Valar\JsonExceptionResponse::class, 'trace' => new Param('debug')],
+
     //'response\redirect' => Valar\Http\RedirectResponse::class,
+    'response\redirect' => Valar\RedirectResponse::class,
 ];
---
+++ src/Http/JsonErrorResponse.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ */
+
+namespace Valar\Http;
+
+use Mvc5\Http\Error;
+
+class JsonErrorResponse
+    extends JsonResponse
+{
+    /**
+     * @param Error $error
+     */
+    function __construct(Error $error)
+    {
+        parent::__construct($error, $error->status());
+    }
+}
