749f063469750bc577c56cd453e9101087c8e29e
devosc <devosc@gmail.com>
2018-06-07 17:33:34 -0500

    Only HttpCookies

--- src/Plugin/Cookies.php
+++ src/Plugin/Cookies.php
@@ -5,7 +5,8 @@
 
 namespace Valar\Plugin;
 
-use Mvc5\Plugin\GlobalVar;
+use Mvc5\Cookie\HttpCookies;
+use Mvc5\Plugin\ScopedCall;
 use Mvc5\Plugin\Shared;
 
 class Cookies
@@ -16,6 +17,16 @@ class Cookies
      */
     function __construct(string $name = 'cookies')
     {
-        parent::__construct($name, new GlobalVar('_COOKIE'));
+        parent::__construct($name, new ScopedCall($this));
+    }
+
+    /**
+     * @return \Closure
+     */
+    function __invoke() : \Closure
+    {
+        return function() {
+            return new HttpCookies($_COOKIE);
+        };
     }
 }
